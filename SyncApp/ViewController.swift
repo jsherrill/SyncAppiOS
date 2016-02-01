26b45274caa61e8318a2d867ab90948a507b93d3
mattfer <mattfer@microsoft.com>
2016-02-01 02:45:41 -0500

    Initial Commit

---
+++ SyncApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SyncApp
+//
+//  Created by Matthew Ferri on 2/1/16.
+//  Copyright © 2016 Matthew Ferri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
