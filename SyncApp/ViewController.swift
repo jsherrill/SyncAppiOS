45d24ec136cc50b85fc770cb4eae306a88e56421
mattfer <mattfer@microsoft.com>
2016-02-04 09:43:52 -0500

    Made it so the userTable is presented first before the messages in the video room.

--- SyncApp/ViewController.swift
+++ SyncApp/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController, YouTubePlayerDelegate, UITableViewDelega
     var roomId: String!
     var youTubeUrl: String!
     
-    var userTableHidden: Bool = true
+    var userTableHidden: Bool = false
 
     // MARK: Creation Methods
     
