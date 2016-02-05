c6a2805a396aa562ac2137c0a2c4096e5a73cff3
Jim S <jimsherrill@jims-macbook-pro-2.local>
2016-02-04 17:51:49 -0700

    Validating Youtube URLs now prior to room creation.

--- SyncApp/CreateRoomViewController.swift
+++ SyncApp/CreateRoomViewController.swift
@@ -8,20 +8,25 @@
 
 import UIKit
 import Firebase
+import YouTubePlayer
 
-class CreateRoomViewController: UIViewController, UITextFieldDelegate {
+class CreateRoomViewController: UIViewController, UITextFieldDelegate, YouTubePlayerDelegate {
 
     @IBOutlet var roomNameTextField: UITextField!
     @IBOutlet weak var youTubeUrlTextField: UITextField!
     @IBOutlet var errorLabel: UILabel!
     var firebaseManager:FirebaseManager!
     var roomId: String!
+    var roomDetails:[String:AnyObject]!
+    var youtubePlayer:YouTubePlayerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         errorLabel.text = ""
+        youtubePlayer = YouTubePlayerView()
+        youtubePlayer.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,12 +39,20 @@ class CreateRoomViewController: UIViewController, UITextFieldDelegate {
         var youTubeUrl: String = youTubeUrlTextField.text!
         
         if roomName.characters.count > 0 && youTubeUrl.characters.count > 0 {
-            let room = firebaseManager.roomsRoot?.childByAutoId()
-            self.roomId = room!.key
-            var roomDetails:[String:AnyObject] = [ "roomName":roomName, "youTubeUrl" : youTubeUrl, "roomId":room!.key]
-            room?.setValue(roomDetails)
+            var url = NSURL(string: youTubeUrl)
+            if url == nil {
+                errorLabel.text = "Error: Please enter a valid Youtube URL"
+            }
+            else {
+                youtubePlayer.loadVideoURL(url!)
+                self.roomDetails = [ "roomName":roomName, "youTubeUrl" : youTubeUrl]
+            }
         }
         else {
+            var errorText = "Error: "
+            if roomName.characters.count == 0 && youTubeUrl.characters.count == 0 {
+                errorText += "Please enter a room name and Youtube URL"
+            }
             errorLabel.text = "Error: Please enter a room name"
         }
     }
@@ -53,6 +66,30 @@ class CreateRoomViewController: UIViewController, UITextFieldDelegate {
         return true
     }
 
+    // MARK: - Youtube Validation
+    func playerReady(videoPlayer: YouTubePlayerView) {
+        if videoPlayer.ready {
+            errorLabel.text = ""
+            let room = firebaseManager.roomsRoot?.childByAutoId()
+            self.roomId = room!.key
+            self.roomDetails["roomId"] = self.roomId
+            
+            room?.setValue(roomDetails)
+            self.performSegueWithIdentifier("idCreatedRoomSegue", sender: self)
+        }
+        else {
+            errorLabel.text = "Error: Please enter a valid Youtube video."
+        }
+    }
+    
+    func playerStateChanged(videoPlayer: YouTubePlayerView, playerState: YouTubePlayerState) {
+        print(playerState)
+    }
+    
+    func playerQualityChanged(videoPlayer: YouTubePlayerView, playbackQuality: YouTubePlaybackQuality) {
+        
+    }
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
